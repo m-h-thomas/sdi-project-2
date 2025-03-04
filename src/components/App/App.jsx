3f614770875d963cf473ab9c61e3ca0b54bedf15
EssEss03 <166960786+EssEss03@users.noreply.github.com>
2025-03-04 16:13:53 -0500

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes, useNavigate } from 'react-router-dom';
 import Home from '../Home/Home.jsx';
 import Episodes from '../Episodes/Episodes.jsx'
-import BoDChalkboard from '../BoDChalkboard/BoDChalkboard'
+import BoDChalkboard from '../BoDChalkboard/BoDChalkboard.jsx'
 
 
 
