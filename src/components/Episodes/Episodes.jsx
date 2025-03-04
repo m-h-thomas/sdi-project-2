6e75530733bb829ca76bbbd1796b5a66e2c894d7
m-h-thomas <mhthomas.8193@gmail.com>
2025-03-04 12:42:33 -0700

    updated style and content of each episode in list

--- src/components/Episodes/Episodes.jsx
+++ src/components/Episodes/Episodes.jsx
@@ -15,29 +15,29 @@ export default function Episodes () {
 
   return (
     <div>
-    <header>
-        <img src="https://raw.githubusercontent.com/whburkle/bobs_burgers/main/img/bobs_burgers_logo.png" />
-        <Link to="/episodes" className="nav-link">Episodes</Link>
-        <Link to="/characters" className="nav-link">Characters</Link>
-    </header>
+      <header>
+          <img src="https://raw.githubusercontent.com/whburkle/bobs_burgers/main/img/bobs_burgers_logo.png" />
+          <Link to="/episodes" className="nav-link">Episodes</Link>
+          <Link to="/characters" className="nav-link">Characters</Link>
+      </header>
 
-    <div>
-      <ul className="episode-list">
-        {episodeList.map((episode) => (
-          <li key={episode.id}>{episode.name}</li>
-        ))}
-      </ul>
+      <div>
+        <ul className="episode-list">
+          {episodeList.map((episode) => (
+            <li key={episode.id}>Season: {episode.season}, Episode: {episode.episode} <div>{episode.name}</div></li>
+          ))}
+        </ul>
+      </div>
+
+      <footer>
+          <div className="imagecarousel">
+              <img src="https://bobsburgers-api.herokuapp.com/images/characters/1.jpg" />
+              <img src="https://bobsburgers-api.herokuapp.com/images/characters/2.jpg" />
+              <img src="https://bobsburgers-api.herokuapp.com/images/characters/3.jpg" />
+              <img src="https://bobsburgers-api.herokuapp.com/images/characters/4.jpg" />
+              <img src="https://bobsburgers-api.herokuapp.com/images/characters/5.jpg" />
+          </div>
+      </footer>
     </div>
-
-    <footer>
-        <div className="imagecarousel">
-            <img src="https://bobsburgers-api.herokuapp.com/images/characters/1.jpg" />
-            <img src="https://bobsburgers-api.herokuapp.com/images/characters/2.jpg" />
-            <img src="https://bobsburgers-api.herokuapp.com/images/characters/3.jpg" />
-            <img src="https://bobsburgers-api.herokuapp.com/images/characters/4.jpg" />
-            <img src="https://bobsburgers-api.herokuapp.com/images/characters/5.jpg" />
-        </div>
-    </footer>
-</div>
   )
 }
