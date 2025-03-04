4bf2d1248fc7cd7e5e7a3e814586003fafb7fca8
joshgore7 <josh.gore77@gmail.com>
2025-03-04 11:47:40 -0500

    stuff

---
+++ src/components/App/App.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, useNavigate } from 'react-router-dom';
+
+// import Characters from '../Characters/Characters';
+
+
+function App() {
+    return(
+      <div>
+        <h1>Hello</h1>
+        <Router>
+          <InnerApp />
+        </Router>
+      </div>
+    )
+  }
+  
+  function InnerApp() {
+    const navigate = useNavigate();
+    return (
+      <>
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/episodes" element={<Episodes />} />
+            {/* <Route path="/characters" element={<Characters />} /> */}
+        </Routes>
+        <button onClick={() => {navigate('/')}}>Home</button>
+        <button onClick={() => {navigate('/episodes')}}>Episodes</button>
+        {/* <button onClick={() => {navigate('/characters')}}>Characters</button> */}
+      </>
+    )
+  }
+export default App;
