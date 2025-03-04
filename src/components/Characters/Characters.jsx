8b45ca1deeef4327f7224b79c971a7927ea24e10
jdj-design <doc_jones12@yahoo.com>
2025-03-04 15:23:18 -0500

    updated characters

--- src/components/Characters/Characters.jsx
+++ src/components/Characters/Characters.jsx
@@ -1,5 +1,35 @@
-function characterList (){
+import React, { useEffect, useState } from 'react';
+
+
+const CharacterPix = ({ image, name, gender }) => (
+  <div className="character-card">
+    <img src={image} alt={name} />
+    <p>{name} ({gender})</p>
+  </div>
+);
+
+const CharacterShots = () => {
+  const [char, setChar] = useState([]);
+
+  useEffect(() => {
     fetch('https://bobsburgers-api.herokuapp.com/characters')
-    .then(res=>res.json())
-    .then()
-}
+      .then(res => res.json())
+      .then(data => setChar(data));
+  }, []); 
+
+  return (
+    <div className='head-shots'>
+      {char.map(character => (
+        <CharacterPix
+          key={character.id}
+          image={character.image}
+          name={character.name}
+          gender={character.gender}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default CharacterShots;
+
