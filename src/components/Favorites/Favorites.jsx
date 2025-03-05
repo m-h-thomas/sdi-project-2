00f03bfcc78fb562d0511a09c9fe5042ed68e524
joshgore7 <josh.gore77@gmail.com>
2025-03-05 17:54:04 -0500

    favorites bar

--- src/components/Favorites/Favorites.jsx
+++ src/components/Favorites/Favorites.jsx
@@ -4,19 +4,18 @@ import './Favorites.css'
 
 export default function Favorites({ favorite=[] }) {
   return (
-    <div className="favorite-list-container">
-      <h2>My Favorite Episodes</h2>
-      {favorite.length > 0 ? (
+    <div className="favorites-carousel">
+      <div className='carousel-content'>
         <ul className="favorite-list">
-          {favorite.map(favEpisode => (
-            <li key={favEpisode.id}>
-              {favEpisode.name} (Season {favEpisode.season}, Episode {favEpisode.episode})
-            </li>
-          ))}
+        {favorite.map(favEpisode => (
+          <li key={favEpisode.id}>
+          {favEpisode.name} <br /> S{favEpisode.season}E{favEpisode.episode}
+          </li>
+        ))}
         </ul>
-      ) : (
-        <h4>No favorite episodes yet.</h4> // If there are no favorites
-      )}
+      </div>
     </div>
-  );
+    );
 }
+
+// {favEpisode.name} (Season {favEpisode.season}, Episode {favEpisode.episode})
