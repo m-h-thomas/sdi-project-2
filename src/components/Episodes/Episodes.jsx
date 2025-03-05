5da9b3cfcdfe503fdb834442b44c038ac0e6d457
joshgore7 <josh.gore77@gmail.com>
2025-03-05 14:09:51 -0500

    some css and homepage stuff

--- src/components/Episodes/Episodes.jsx
+++ src/components/Episodes/Episodes.jsx
@@ -1,7 +1,6 @@
 import { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import "./Episodes.css";
-import NavBar from "../NavBar/NavBar";
 import FavoriteButton from "../Favorites/FavoriteButton";
 
 export default function Episodes() {
@@ -29,10 +28,6 @@ export default function Episodes() {
 
   return (
     <div>
-      <header>
-        <NavBar />
-      </header>
-
       <div>
         <h1>Episode List</h1>
 
@@ -53,16 +48,6 @@ export default function Episodes() {
           </div>
         ))}
       </div>
-
-      <footer>
-        <div className="imagecarousel">
-          <img src="https://bobsburgers-api.herokuapp.com/images/characters/1.jpg" />
-          <img src="https://bobsburgers-api.herokuapp.com/images/characters/2.jpg" />
-          <img src="https://bobsburgers-api.herokuapp.com/images/characters/3.jpg" />
-          <img src="https://bobsburgers-api.herokuapp.com/images/characters/4.jpg" />
-          <img src="https://bobsburgers-api.herokuapp.com/images/characters/5.jpg" />
-        </div>
-      </footer>
     </div>
   );
 }
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,39 +1,18 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import BoDChalkboard from '../BoDChalkboard/BoDChalkboard.jsx'
 import "./Home.css";
-import NavBar from "../NavBar/NavBar";
 
-function Home(){
+export default function Home(){
     return(
         <div className="home-body">
-            <header>
-                <NavBar/>
-            </header>
             <div className="main-container">
                 <div className="main-text">
-                    <p>Sample Text</p><br />
-                    <p>Episode 5</p><br />
-                    <p>Whatever the title is</p><br />
                 </div>
             </div>
-
-            <footer>
-                <div className="imagecarousel">
-                    <img src="https://bobsburgers-api.herokuapp.com/images/characters/1.jpg" />
-                    <img src="https://bobsburgers-api.herokuapp.com/images/characters/2.jpg" />
-                    <img src="https://bobsburgers-api.herokuapp.com/images/characters/3.jpg" />
-                    <img src="https://bobsburgers-api.herokuapp.com/images/characters/4.jpg" />
-                    <img src="https://bobsburgers-api.herokuapp.com/images/characters/5.jpg" />
-                    <img src="https://bobsburgers-api.herokuapp.com/images/characters/1.jpg" />
-                    <img src="https://bobsburgers-api.herokuapp.com/images/characters/2.jpg" />
-                    <img src="https://bobsburgers-api.herokuapp.com/images/characters/1.jpg" />
-                    <img src="https://bobsburgers-api.herokuapp.com/images/characters/2.jpg" />
-                    <img src="https://bobsburgers-api.herokuapp.com/images/characters/3.jpg" />
-
-                </div>
-            </footer>
+            <div className="chalkboard-container">
+                <BoDChalkboard />
+            </div>
         </div>
     )
 }
 
-export default Home;
