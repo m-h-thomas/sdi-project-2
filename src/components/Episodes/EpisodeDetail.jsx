14aa97a17964a1ae32bb94823876be22728b6e5d
m-h-thomas <mhthomas.8193@gmail.com>
2025-03-06 10:07:50 -0700

    changes to EpisodeDetail to try to align styling with Home page

--- src/components/Episodes/EpisodeDetail.jsx
+++ src/components/Episodes/EpisodeDetail.jsx
@@ -1,36 +1,106 @@
 import { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
-import NavBar from '../NavBar/NavBar'
+import '../Home/Home.css'
+import '../Home/MainEpisodeDisplay.css'
+
+const baseURL = "https://bobsburgers-api.herokuapp.com";
 
 export default function EpisodeDetail () {
 
   const [ episodeDetails, setEpisodeDetails ] = useState([])
+  const [ pestControl, setPestControl ] = useState(null);
+  const [ burgerOfTheDay, setBurgerOfTheDay ] = useState(null);
+  const [ storeNextDoor, setStoreNextDoor ] = useState(null);
   const { id } = useParams()
 
   useEffect(() => {
-    fetch(`https://bobsburgers-api.herokuapp.com/episodes/${id}`)
+    fetch(`${baseURL}/episodes/${id}`)
       .then(res => res.json())
       .then(data => setEpisodeDetails(data))
+  }, [id])
+
+  useEffect(() => {
+    fetch(`${baseURL}/pestControlTruck/${id}`)
+      .then(res => res.json())
+      .then(data => setPestControl(data))
+  }, [id])
+
+  useEffect(() => {
+    fetch(`${baseURL}/storeNextDoor/${id}`)
+      .then(res => res.json())
+      .then(data => setStoreNextDoor(data))
+  }, [id])
+
+  useEffect(() => {
+    fetch(`${baseURL}/burgerOfTheDay/${id}`)
+      .then(res => res.json())
+      .then(data => setBurgerOfTheDay(data))
   }, [id])
 
 
   return (
     <>
-      <NavBar />
+      <div className="home-body">
+            <div className="main-container">
 
-      <div className="episode-detail-container">
+                <div className="episode-container">
+                  <div className="episode-text">
+                    {setEpisodeDetails && (
+                      <div className="episode-box">
+                        <p>
+                          <strong>{episodeDetails.name}</strong>
+                        </p>
+                        <p>
+                          <em>Aired:</em> {episodeDetails.airDate}
+                        </p>
+                      </div>
+                    )}
 
-      <h1>{episodeDetails.name}</h1>
-        <h2>Season: {episodeDetails.season} Episode: {episodeDetails.episode}</h2>
-        <h2>Episode Description</h2>
-          <p>{episodeDetails.description}</p>
-        <h2>Original Air Date</h2>
-          <p>{episodeDetails.airDate}</p>
-        <h2>Total Viewers</h2>
-          <p>{episodeDetails.totalViewers}</p>
+                    {burgerOfTheDay && (
+                      <div className="episode-section-burger">
+                        <p>
+                          <strong>🍔 Burger of The Day:</strong> {burgerOfTheDay.name}
+                        </p>
+                      </div>
+                    )}
+                  </div>
+
+                  <div className="images">
+                    {pestControl && (
+                      <div className="episode-section-pest">
+                        <p>
+                          <strong>Pest Control:</strong>
+                        </p>
+                        {pestControl.image && (
+                          <img src={pestControl.image} alt="Pest Control" />
+                        )}
+                      </div>
+                    )}
+
+                    {storeNextDoor && (
+                      <div className="episode-section-store">
+                        <p>
+                          <strong>Store Next Door:</strong>
+                        </p>
+                        {storeNextDoor.image && (
+                          <img src={storeNextDoor.image} alt="Store Next Door" />
+                        )}
+                      </div>
+                    )}
+                  </div>
+
+                  <div className="wiki-link">
+                    <a href={episodeDetails.wikiUrl} target="_blank" rel="noopener noreferrer">
+                      Even More Details at the Wiki
+                    </a>
+                  </div>
+
+                </div>
+
+            </div>
+
       </div>
-      <div><a href={episodeDetails.wikiUrl} target="_blank" rel="noopener noreferrer">Even More Details at the Wiki</a></div>
-
     </>
+
   )
 }
