0054be3f690ab6db0e96c7b997993d676860fa85
joshgore7 <josh.gore77@gmail.com>
2025-03-06 16:46:15 -0500

    idk

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -16,9 +16,6 @@ export default function NavBar(){
                 <Link to="/characters" className="nav-link">
                     Characters
                 </Link>
-
-
-
             </div>
         </div>
     )
